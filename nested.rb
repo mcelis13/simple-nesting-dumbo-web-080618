f62fb3e7f7741c3594a0ca4982d68496e956362a
Madeline Celis <madelinebcelis@gmail.com>
2018-07-24 01:03:24 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -121,7 +121,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-		 programmer_hash[:dennis_ritchie][:languages] = "Assembly"
+		 programmer_hash[:dennis_ritchie][:languages][1] = "Assembly"
 		 puts programmer_hash
 end
 adding_to_dennis
